9a285df08b6d9dbc36f770267e1d19cdf6828709
Askyras <ellery@otyx.com>
2022-12-16 16:21:41 +0100

    fast replace

--- data-updates.lua
+++ data-updates.lua
@@ -2,7 +2,7 @@
 local boiler = util.table.deepcopy(data.raw["assembling-machine"]["se-electric-boiler"])
 boiler.name = "se-surge-electric-boiler"
 boiler.minable = {mining_time = 0.5, result = "se-surge-electric-boiler"}
-
+boiler.fast_replaceable_group = "electric-boiler"
 boiler.energy_source = {
     type = "electric",
     usage_priority = "tertiary"
@@ -19,11 +19,13 @@ boiler_recipe = {
 	enabled = false,
 }
 
-table.insert(data.raw["technology"]["se-electric-boiler"].effects, {type = "unlock-recipe", recipe = boiler_recipe.name,} )
-
 local boiler_item = util.table.deepcopy(data.raw["item"]["se-electric-boiler"])
 boiler_item.name = "se-surge-electric-boiler"
 boiler_item.place_result = "se-surge-electric-boiler"
+
+table.insert(data.raw["technology"]["se-electric-boiler"].effects, {type = "unlock-recipe", recipe = boiler_recipe.name,} )
+
+data.raw["assembling-machine"]["se-electric-boiler"].fast_replaceable_group = "electric-boiler"
 
 data:extend{
 boiler,
